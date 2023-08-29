b39197a98941ceb7ac6d7d29ad80bce47f3ab1db
lotekone <joar@dedypeskoger>
2023-08-29 20:51:46 +0200

    client form, loading ui a6 suspense

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 async function getTickets() {
+  // imitate a slow network connection
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
       revalidate: 0, // use 0 to opt out of using cache
