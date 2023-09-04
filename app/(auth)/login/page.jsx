78b11d94c860743b420bf642e7ca3cc987c5c1b9
lotekone <joar@dedypeskoger>
2023-09-04 21:34:37 +0200

    add auth forms

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 export default function login() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("login page", email, password);
+  };
   return (
-    <main>
-      <h2 className="text-center">Log in</h2>
+    <main className="text-center">
+      <h2>Log in</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 export default function signup() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("Signup page:", email, password);
+  };
   return (
     <main className="text-center">
       <h2>signup</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
